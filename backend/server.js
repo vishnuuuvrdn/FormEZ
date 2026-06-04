b99209148aa665ae62a09f24a7f010352584b3b0
vishnuuuvrdn <vishnuvardhan9376@gmail.com>
2026-06-04 17:28:47 +0530

    fix: cors origin allowed

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,10 @@ connectDB();
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: "https://formez.pages.dev",
+  credentials : true  
+}));
 app.use(express.json());
 
 app.use("/api/documents", documentRoutes);
